fe58e10ed7588cdbb54ddb9fda731dd42968cda3
Chathra Navoda Ranasinghe <91416868+ChathraNavoda@users.noreply.github.com>
2023-03-04 17:36:26 +0530

    user registration

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,67 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
 
 import 'package:taskmate/screens/add_tasks_screen.dart';
 import 'package:taskmate/screens/auth/login.dart';
 import 'package:taskmate/screens/update_screen.dart';
 import 'screens/home_screen.dart';
 
-Future<void> main() async {
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'TaskMate',
+//       routes: {
+//         '/': (context) => const HomeScreen(),
+//         '/add': (context) => const AddTaskScreen(),
+//         '/update': (context) => const UpdateTaskScreen(),
+//         '/login': (context) => Login(),
+//       },
+//       initialRoute: '/login',
+//     );
+//   }
+// }
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TaskMate',
-      routes: {
-        '/': (context) => const HomeScreen(),
-        '/add': (context) => const AddTaskScreen(),
-        '/update': (context) => const UpdateTaskScreen(),
-        '/login': (context) => Login(),
-      },
-      initialRoute: '/login',
-    );
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for Errors
+          if (snapshot.hasError) {
+            print("Something Went Wrong");
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return MaterialApp(
+            title: 'TaskMate',
+            routes: {
+             // '/': (context) => const HomeScreen(),
+              '/add': (context) => const AddTaskScreen(),
+              '/update': (context) => const UpdateTaskScreen(),
+              '/login': (context) => Login(),
+            },
+            initialRoute: '/login',
+          );
+        });
   }
 }
--- lib/screens/auth/signup.dart
+++ lib/screens/auth/signup.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:taskmate/constants/global_variables.dart';
 import 'package:taskmate/screens/auth/login.dart';
 
 class Signup extends StatefulWidget {
@@ -29,71 +31,73 @@ class _SignupState extends State<Signup> {
     super.dispose();
   }
 
-  // registration() async {
-  //   if (password == confirmPassword) {
-  //     try {
-  //       UserCredential userCredential = await FirebaseAuth.instance
-  //           .createUserWithEmailAndPassword(email: email, password: password);
-  //       print(userCredential);
-  //       ScaffoldMessenger.of(context).showSnackBar(
-  //         SnackBar(
-  //           backgroundColor: Colors.redAccent,
-  //           content: Text(
-  //             "Registered Successfully. Please Login..",
-  //             style: TextStyle(fontSize: 20.0),
-  //           ),
-  //         ),
-  //       );
-  //       Navigator.pushReplacement(
-  //         context,
-  //         MaterialPageRoute(
-  //           builder: (context) => Login(),
-  //         ),
-  //       );
-  //     } on FirebaseAuthException catch (e) {
-  //       if (e.code == 'weak-password') {
-  //         print("Password Provided is too Weak");
-  //         ScaffoldMessenger.of(context).showSnackBar(
-  //           SnackBar(
-  //             backgroundColor: Colors.orangeAccent,
-  //             content: Text(
-  //               "Password Provided is too Weak",
-  //               style: TextStyle(fontSize: 18.0, color: Colors.black),
-  //             ),
-  //           ),
-  //         );
-  //       } else if (e.code == 'email-already-in-use') {
-  //         print("Account Already exists");
-  //         ScaffoldMessenger.of(context).showSnackBar(
-  //           SnackBar(
-  //             backgroundColor: Colors.orangeAccent,
-  //             content: Text(
-  //               "Account Already exists",
-  //               style: TextStyle(fontSize: 18.0, color: Colors.black),
-  //             ),
-  //           ),
-  //         );
-  //       }
-  //     }
-  //   } else {
-  //     print("Password and Confirm Password doesn't match");
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       SnackBar(
-  //         backgroundColor: Colors.orangeAccent,
-  //         content: Text(
-  //           "Password and Confirm Password doesn't match",
-  //           style: TextStyle(fontSize: 16.0, color: Colors.black),
-  //         ),
-  //       ),
-  //     );
-  //   }
-  // }
+  registration() async {
+    if (password == confirmPassword) {
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(email: email, password: password);
+        print(userCredential);
+        // ignore: use_build_context_synchronously
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            backgroundColor: Colors.green,
+            content: Text(
+              "Registered Successfully. Please Login..",
+              style: TextStyle(fontSize: 20.0),
+            ),
+          ),
+        );
+        // ignore: use_build_context_synchronously
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Login(),
+          ),
+        );
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          print("Password Provided is too Weak");
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              backgroundColor: Colors.orangeAccent,
+              content: Text(
+                "Password Provided is too Weak",
+                style: TextStyle(fontSize: 18.0, color: Colors.black),
+              ),
+            ),
+          );
+        } else if (e.code == 'email-already-in-use') {
+          print("Account Already exists");
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              backgroundColor: Colors.orangeAccent,
+              content: Text(
+                "Account Already exists",
+                style: TextStyle(fontSize: 18.0, color: Colors.black),
+              ),
+            ),
+          );
+        }
+      }
+    } else {
+      print("Password and Confirm Password doesn't match");
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          backgroundColor: Colors.orangeAccent,
+          content: Text(
+            "Password and Confirm Password doesn't match",
+            style: TextStyle(fontSize: 16.0, color: Colors.black),
+          ),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("User SignUp"),
+        title: const Text("User SignUp"),
       ),
       body: Form(
         key: _formKey,
@@ -102,10 +106,10 @@ class _SignupState extends State<Signup> {
           child: ListView(
             children: [
               Container(
-                margin: EdgeInsets.symmetric(vertical: 10.0),
+                margin: const EdgeInsets.symmetric(vertical: 10.0),
                 child: TextFormField(
                   autofocus: false,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: 'Email: ',
                     labelStyle: TextStyle(fontSize: 20.0),
                     border: OutlineInputBorder(),
@@ -124,11 +128,11 @@ class _SignupState extends State<Signup> {
                 ),
               ),
               Container(
-                margin: EdgeInsets.symmetric(vertical: 10.0),
+                margin: const EdgeInsets.symmetric(vertical: 10.0),
                 child: TextFormField(
                   autofocus: false,
                   obscureText: true,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: 'Password: ',
                     labelStyle: TextStyle(fontSize: 20.0),
                     border: OutlineInputBorder(),
@@ -145,11 +149,11 @@ class _SignupState extends State<Signup> {
                 ),
               ),
               Container(
-                margin: EdgeInsets.symmetric(vertical: 10.0),
+                margin: const EdgeInsets.symmetric(vertical: 10.0),
                 child: TextFormField(
                   autofocus: false,
                   obscureText: true,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: 'Confirm Password: ',
                     labelStyle: TextStyle(fontSize: 20.0),
                     border: OutlineInputBorder(),
@@ -165,50 +169,46 @@ class _SignupState extends State<Signup> {
                   },
                 ),
               ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        // Validate returns true if the form is valid, otherwise false.
-                        if (_formKey.currentState!.validate()) {
-                          setState(() {
-                            email = emailController.text;
-                            password = passwordController.text;
-                            confirmPassword = confirmPasswordController.text;
-                          });
-                          // registration();
-                        }
-                      },
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(fontSize: 18.0),
-                      ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Validate returns true if the form is valid, otherwise false.
+                      if (_formKey.currentState!.validate()) {
+                        setState(() {
+                          email = emailController.text;
+                          password = passwordController.text;
+                          confirmPassword = confirmPasswordController.text;
+                        });
+                        registration();
+                      }
+                    },
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(fontSize: 18.0),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Already have an Account? "),
-                    TextButton(
-                        onPressed: () => {
-                              Navigator.pushReplacement(
-                                context,
-                                PageRouteBuilder(
-                                  pageBuilder:
-                                      (context, animation1, animation2) =>
-                                          Login(),
-                                  transitionDuration: Duration(seconds: 0),
-                                ),
-                              )
-                            },
-                        child: Text('Login'))
-                  ],
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Already have an Account? "),
+                  TextButton(
+                      onPressed: () => {
+                            Navigator.pushReplacement(
+                              context,
+                              PageRouteBuilder(
+                                pageBuilder:
+                                    (context, animation1, animation2) =>
+                                        Login(),
+                                transitionDuration: const Duration(seconds: 0),
+                              ),
+                            )
+                          },
+                      child: const Text('Login'))
+                ],
               )
             ],
           ),
