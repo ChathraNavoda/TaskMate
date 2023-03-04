f6c2fa30ed759989da3cba8070b1891a5462594f
Chathra Navoda Ranasinghe <91416868+ChathraNavoda@users.noreply.github.com>
2023-03-04 09:33:32 +0530

    fetch data from db

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:taskmate/screens/add_tasks_screen.dart';
 import 'screens/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:taskmate/constants/global_variables.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,6 +11,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final CollectionReference task =
+      FirebaseFirestore.instance.collection('Task');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,6 +54,71 @@ class _HomeScreenState extends State<HomeScreen> {
         child: const Icon(Icons.add_outlined),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: StreamBuilder(
+          stream: task.snapshots(),
+          builder: (context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    final DocumentSnapshot taskSnap = snapshot.data.docs[index];
+                    return Container(
+                      height: 80,
+                      color: Colors.white,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            child: CircleAvatar(
+                              backgroundColor: GlobalVariables.darkGreenColor,
+                              radius: 40,
+                              child: Text(
+                                taskSnap['status'],
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                taskSnap['title'],
+                                style: const TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                taskSnap['description'],
+                                style: const TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.w500),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.edit_note,
+                                  color: Color.fromARGB(255, 239, 209, 15),
+                                  size: 35,
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.delete,
+                                  color: Color.fromARGB(255, 159, 12, 12),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            }
+            return Container();
+          }),
     );
   }
 }
